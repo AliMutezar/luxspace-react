89fe539f03ae8c57ce67961321edafd95d93c6f4
AliMutezar <aamutezar@gmail.com>
2025-10-24 11:22:37 +0700

    update just arrived n browse the room section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import BrowseRoom from '../parts/HomePage/BrowseRoom'
+import JustArrived from '../parts/HomePage/JustArrived'
 
 
 export default function HomePage() {
@@ -8,6 +10,8 @@ export default function HomePage() {
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
+      <JustArrived />
     </>
   )
 }
